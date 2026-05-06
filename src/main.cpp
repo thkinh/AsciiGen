afb2c877c9ad45a40960bf359f3d0db4a7024969
thkinh <139554989+thkinh@users.noreply.github.com.>
2026-05-06 16:06:21 +0700

    add resize in cli arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstdlib>
 #include <iostream>
 #include "PixelBuffer.h"
 #include "ImageLoader.h"
@@ -11,11 +12,25 @@ int main(int argc, char** argv) {
     PNGImageLoader loader = PNGImageLoader();
 
     const char* file_name;
-
+    size_t blockWidth = 0;
+    size_t blockHeight = 0;
     if (argc > 1) {
         file_name = argv[1];
     } else {
         file_name = "../assets/original-apple.png";
+    }
+
+    if(argc > 2) {
+        blockWidth  = atoi(argv[2]);
+        blockHeight = atoi(argv[3]);
+    } else {
+        blockWidth = 4;
+        blockHeight = 8;
+    }
+
+    if(blockWidth == 0 || blockHeight == 0) {
+        std::cout << "wrong block size\n";
+        return 1;
     }
 
     if(!loader.load(file_name, buf))
@@ -25,18 +40,17 @@ int main(int argc, char** argv) {
     }
     buf.preview(50);
 
-    size_t interval = 90;
+    size_t interval = 30;
     AsciiConverter converter(interval);
 
     //Register ascii chars
     //std::string charList = "@WMBQGNRD$S&%E8gmHwA#K96CUZPXdqbp5023Vae4FhokYsynTcJ[]z7L?xv{}1f>j<t()=I|+lr!i^/\\\"*~;_' :, -`.";
-    std::string charList = "@WMe4Fho -`.";
+    std::string charList = "@ ";
     converter.registerAsciiList(charList);
 
     //Draw the buffer to the canvas
-    size_t blockWidth  = 4;
-    size_t blockHeight = 8;
     AsciiCanvas canvas = AsciiCanvas(buf.width()/blockWidth, buf.height()/blockHeight);
+    std::cout << "Final w/h " << buf.width()/blockWidth << " " << buf.height()/blockHeight << "\n";
     if(converter.convertWholeCanvasByBlock(canvas, buf, blockWidth, blockHeight))
     {
         canvas.render(false);
